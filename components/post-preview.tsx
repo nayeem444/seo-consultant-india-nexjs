0bce5a79174e0f25f9f59030bcb4af337fa5e111
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-02-14 12:07:04 +0530

    blog fix

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -20,7 +20,7 @@ export default function PostPreview({
       </div> */}
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
-          href={`/posts/${slug}`}
+          href={`/blog/${slug}`}
           className="hover:underline text-blue-600 font-bold "
           dangerouslySetInnerHTML={{ __html: title }}
         ></Link>
